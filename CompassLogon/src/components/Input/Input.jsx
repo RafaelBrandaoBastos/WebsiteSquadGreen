4b23ba7baac5ed97c5462e08d4cc3d3caae641fd
Rafael Brandão <rafaelbrandaobastos@gmail.com>
2022-06-29 16:17:19 -0300

    Chore: Functional  Local Storage

--- CompassLogon/src/components/Input/Input.jsx
+++ CompassLogon/src/components/Input/Input.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {InputUser, InputPassword} from "./Input.Styled"
 
-const Input = ({type, placeholder, register, className})=>{
+
+const Input = ({type, placeholder, register, className, inputdata})=>{
+  
+  
 
   if (placeholder=="Usuário")
   return(   
   
-  <InputUser type={type} placeholder={placeholder} {...register} className={className} />
+  <InputUser type={type} defaultValue={inputdata} editable={true} placeholder={placeholder} {...register} className={className} />
 
   )
   if (placeholder=="Senha")
   return(
-  <InputPassword type={type} placeholder={placeholder} {...register} className={className}/>
+  <InputPassword type={type} defaultValue={inputdata} editable={true} placeholder={placeholder} {...register} className={className}/>
 
   )
 };
--- CompassLogon/src/pages/Login/Login.jsx
+++ CompassLogon/src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 import {Body, Picture, Main, ContainerIcon, Interface, Form, Image, Tittle, Text, Icon} from "./Login.Styled.js"
-import React from 'react'
+import React, {useState, useEffect} from "react";
 import ButtonStyled from "../../components/Button/Button"
 import {ErrorMessage, Label, ContainerInput} from "../../components/Input/Input.Styled"
 import Input from "../../components/Input/Input"
@@ -13,8 +13,21 @@ let navigate = useNavigate();
 
 function validation(userData) {
   console.log(userData);
+  localStorage.setItem("userData.user", userData.user);
+  localStorage.setItem("userData.password", userData.password);
   navigate('/sucess');
 }
+
+  let [datauser, setDatauser] = useState(localStorage.getItem("userData.user"));
+  let [datapassword, setDatapassword] = useState(localStorage.getItem("userData.password")); 
+  
+  const handleChangeuser = event => {
+    setDatauser(event.target.value);}
+
+  const handleChangepassword = event => {
+      setDatapassword(event.target.value);}
+
+
   return (
     <Body> 
       <Main>
@@ -26,9 +39,9 @@ function validation(userData) {
           <Text>Para continuar navegando de forma segura, efetue o login na rede</Text> 
           <Form onSubmit={handleSubmit(validation)}>
             <Label htmlFor="Usuário">Login</Label>
-            <Input type="text" placeholder="Usuário" className={errors.password || errors.user?"invalid": " "} {...{register:register("user", {required:true, pattern: /^[a-z]{2,}[.][a-z]{2,}$/})}}/>
+            <Input type="text" placeholder="Usuário" inputdata = {datauser} onChange = {handleChangeuser} className={errors.password || errors.user?"invalid": " "} {...{register:register("user", {required:true, pattern: /^[a-z]{2,}[.][a-z]{2,}$/})}}/>
           <ContainerInput>
-            <Input type="password" placeholder="Senha" className={errors.password || errors.user?"invalid": " "} {...{register:register("password", {required:true, pattern: /^[0-9]{6,9}$/})}} /> 
+            <Input type="password" placeholder="Senha" inputdata = {datapassword} onChange = {handleChangepassword} className={errors.password || errors.user?"invalid": " "} {...{register:register("password", {required:true, pattern: /^[0-9]{6,9}$/})}} /> 
             {errors.password && <ErrorMessage>Ops, usuário ou senha inválidos.<br/>Tente novamente!</ErrorMessage> || errors.user && <ErrorMessage>Ops, usuário ou senha inválidos.<br/>Tente novamente!</ErrorMessage>}
           </ContainerInput>
             <ButtonStyled name="btn" type="Submit"/> 
