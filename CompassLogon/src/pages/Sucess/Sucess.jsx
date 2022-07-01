5e6ebf1f05494fad01320370ba84fb7ebbcabaa9
Victor Hugo <victor.vivtorhugo@gmail.com>
2022-07-01 19:01:44 -0300

    Bug: BugFix from the router 401

--- CompassLogon/src/pages/Sucess/Sucess.jsx
+++ CompassLogon/src/pages/Sucess/Sucess.jsx
@@ -1,10 +1,5 @@
-import React from 'react'
-
+import React from "react";
 
 export default function Sucess() {
-  
-  return (
-    <div>Sucess</div>
-  )
+  return <div>Sucess</div>;
 }
-
--- CompassLogon/src/utils/router.jsx
+++ CompassLogon/src/utils/router.jsx
@@ -5,13 +5,11 @@ import ErrorPage401 from "../pages/Errors/ErrorPage401/ErrorPage401";
 import Login from "../pages/Login/Login";
 import Sucess from "../pages/Sucess/Sucess";
 
-
 export default function MainRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Login />}/>
-      <Route path="/sucess" element={ 
-           <ErrorPage401 isAllowed={localStorage.getItem("userData.user") != null && localStorage.getItem("userData.password") !=null}/>}/>
+      <Route path="/" element={<Login />} />
+      <Route path="/sucess" element={<ErrorPage401 />} />
       <Route path="*" element={<ErrorPage404 />} />
     </Routes>
   );
